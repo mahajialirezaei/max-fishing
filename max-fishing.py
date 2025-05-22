95166f7d971cce92a80cb5bf55b3a79882a798b1
mahajialirezaei <m.a.hajialirezaei05@gmail.com>
2025-05-22 17:56:42 +0330

    update check

--- max-fishing.py
+++ max-fishing.py
@@ -11,7 +11,7 @@ def start(grid, m, n):
 
 
 def check(grid, i, j, visited):
-    return i>=0 and j>=0 and i < len(grid) and j < len(grid[i]) and tuple([i, j]) not in visited
+    return i>=0 and j>=0 and i < len(grid) and j < len(grid[i]) and tuple([i, j]) not in visited and grid[i][j]!= 0
 
 
 def maxFishing(grid, m, n, i, j, visited, cur):
@@ -37,3 +37,17 @@ def maxFishing(grid, m, n, i, j, visited, cur):
         cur += grid[i][j - 1]
         maxFishing(grid, m, n, i, j - 1, visited, cur)
         cur -= grid[i][j - 1]
+
+
+
+grid = [
+[0, 2, 1, 0],
+[4, 0, 0, 3],
+[1, 0, 0, 4],
+[0, 3, 2, 0]
+]
+
+m = 4
+n = 4
+start(grid, m, n)
+print(maximumOfProblem)
