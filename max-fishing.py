e402533b78444a089cede5d74cbd35be0d47354e
mahajialirezaei <m.a.hajialirezaei05@gmail.com>
2025-05-22 17:52:01 +0330

    implement maxFishing function

---
+++ max-fishing.py
@@ -0,0 +1,39 @@
+maximumOfProblem = 0
+
+
+def start(grid, m, n):
+    for i in range(m):
+        for j in range(n):
+            visited = set()
+            if grid[i][j] != 0:
+                visited.add(tuple([i, j]))
+                maxFishing(grid, m, n, i, j, visited, grid[i][j])
+
+
+def check(grid, i, j, visited):
+    pass
+
+
+def maxFishing(grid, m, n, i, j, visited, cur):
+    global maximumOfProblem
+    maximumOfProblem = max(maximumOfProblem, cur)
+    if check(grid, i + 1, j, visited):
+        visited.add(tuple([i + 1, j]))
+        cur += grid[i + 1][j]
+        maxFishing(grid, m, n, i + 1, j, visited, cur)
+        cur -= grid[i + 1][j]
+    if check(grid, i, j + 1, visited):
+        visited.add(tuple([i, j + 1]))
+        cur += grid[i][j + 1]
+        maxFishing(grid, m, n, i, j + 1, visited, cur)
+        cur -= grid[i][j + 1]
+    if check(grid, i - 1, j, visited):
+        visited.add(tuple([i - 1, j]))
+        cur += grid[i - 1][j]
+        maxFishing(grid, m, n, i - 1, j, visited, cur)
+        cur -= grid[i - 1][j]
+    if check(grid, i, j - 1, visited):
+        visited.add(tuple([i, j - 1]))
+        cur += grid[i][j - 1]
+        maxFishing(grid, m, n, i, j - 1, visited, cur)
+        cur -= grid[i][j - 1]
